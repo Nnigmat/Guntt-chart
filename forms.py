591b6df0d969fd6eeddcf601990ba112e60acf63
Nikita <nnigmat@gmail.com>
2019-04-24 18:32:09 +0300

    Add validation of start and end dates

--- forms.py
+++ forms.py
@@ -7,3 +7,10 @@ class EventForm(FlaskForm):
     event = StringField('event', validators=[DataRequired()])
     start = DateField('starting date', format='%d.%m.%Y', validators=[DataRequired()])
     end = DateField('ending date', format='%d.%m.%Y', validators=[DataRequired()])
+
+    def validate_on_submit(self):
+        result = super(EventForm, self).validate()
+        if (self.start.data > self.end.data):
+            return False
+        else:
+            return result
