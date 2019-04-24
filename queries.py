2892c5dd860938b1545f8dcadcf2b588243acab2
Almir Mullanurov <almirka02261999@gmail.com>
2019-04-24 19:43:57 +0300

    Added queries for the database

---
+++ queries.py
@@ -0,0 +1,17 @@
+import datetime
+# Select event with start_date equal to date
+def query1(cur, date):
+    date.strftime("%d.%m.%Y")
+    cur.execute(f'''SELECT * FROM event WHERE (data->>'start_date') = ''{date}'' ''')
+    return cur.fetchall()
+
+# Select all event and sort them
+def query2(cur):
+    cur.execute(f'''SELECT * FROM event ORDER BY (data->>'start_date')''')
+    return cur.fetchall()
+
+# Select event with date between start_date and end_date
+def query3(cur, date):
+    date.strftime("%d.%m.%Y")
+    cur.execute(f'''SELECT * FROM event WHERE (data->>'start_date') <= ''{date}'' AND (data->>'end_date') >= ''{date}'' ''')
+    return cur.fetchall()
